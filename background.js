37da99684e08d9ec19cefd0552d06cf1ffc09bce
sugi511 <yoshinori.sugiyama@social-recruiting.asia>
2013-04-19 20:27:12 +0900

    いけてるぅ！！

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // ブラウザーアクションで、アイコンがクリックされたときのリスナー
 chrome.browserAction.onClicked.addListener(function(tab) {
-	// タブの中でスクリプトを十個すする
-	chrome.tabs.executeScript(null, {file: "content.js"}, function(){
+	// タブの中でスクリプトを実行する
+	chrome.tabs.executeScript(null, {file: "./content.js"}, function(){
 	});
 });
---
+++ content.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("a").each(function(){
+		if($(this).text()=="いいね！"){
+			$(this).text("いけてるぅ！！");
+		}
+	});
+});
