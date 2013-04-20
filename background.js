45f8476ba1b8c187d9aa5491229c04d316e3d696
Tagami Daiki <daiki.tagami@social-recruiting.asia>
2013-04-20 13:05:16 +0900

    first commit

--- background.js
+++
@@ -1,6 +0,0 @@
-// ブラウザーアクションで、アイコンがクリックされたときのリスナー
-chrome.browserAction.onClicked.addListener(function(tab) {
-	// タブの中でスクリプトを実行する
-	chrome.tabs.executeScript(null, {file: "./content.js"}, function(){
-	});
-});
--- content.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$("a").each(function(){
-		if($(this).text()=="いいね！"){
-			$(this).text("いけてるぅ！！！");
-		}
-	});
-});
